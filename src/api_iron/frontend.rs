6e2fc7becfcf340078cdaba578432a98c80a10c9
Augustus Mayo <gusmayo@gmail.com>
2018-05-11 07:24:09 -0500

    Remove old iron impl

--- src/api_iron/frontend.rs
+++
@@ -1,7 +0,0 @@
-use staticfile::Static;
-
-use std::path::Path;
-
-pub fn v1() -> Static {
-    Static::new(Path::new("src/frontend/static/"))
-}
--- src/api_iron/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod api;
-mod backend;
-pub mod error;
-mod flag;
-pub mod frontend;
-mod path;
-mod status;
